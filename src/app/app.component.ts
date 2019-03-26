93f07a45bef6cf6cef113423c02167cd204d02d7
Test Man <ttestman4@gmail.com>
2019-03-26 08:35:59 +0530

    virtual scroll sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spa';
+
+  items = Array.from({ length: 10000 }).map((_, i) =>
+    ({
+      msg1: `Msg1 #${i}`,
+      msg2: `Msg2 #${i}`,
+      id: i
+    }));
 }
